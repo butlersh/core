41b24f6098998825c48c18714953b93a7fc38ca7
Quynh Nguyen <seriquynh@gmail.com>
2024-11-12 10:59:17 +0700

    Check the OS when installing MySQL

--- scripts/mysql-setup.sh
+++ scripts/mysql-setup.sh
@@ -3,9 +3,20 @@
 # ./mysql-setup.sh --version=8.0
 
 if [ "$USER" != 'root' ]; then
-    echo '[error] root privileges required. Please run this script as root.'
+    echo '[forge.ERROR]: root privileges required. Please run this script as root.'
     exit 1
 fi
+
+OS_DESC="$(lsb_release -a | grep 'Description' | awk '{print $2 " "  $3}')"
+
+case "$OS_DESC" in
+    "Ubuntu 24.04")
+        # If the OS is supported, it just continue this script.
+    ;;
+    *)
+        echo "[forge.ERROR] Unsupported OS, currently supported Ubuntu 24.04"
+    ;;
+esac
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -22,12 +33,30 @@ do
     if [ "$NAME" = '--version' ]; then
         F_MYSQL_VERSION="$VALUE"
     else
-        echo "[error] Unrecognized option $NAME"
+        echo "[forge.ERROR] Unrecognized argument/option $NAME."
         exit 1
     fi
 done
 
-debconf-set-selections <<< "mysql-server mysql-server/root_password password ${F_MYSQL_PASSWORD}"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${F_MYSQL_PASSWORD}"
+mysql80 () {
+    echo "[forge.INFO] Start installing MySQL $F_MYSQL_VERSION"
 
-apt-get -y install mysql-server
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password ${F_MYSQL_PASSWORD}"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${F_MYSQL_PASSWORD}"
+
+    apt-get -y install mysql-server
+
+    echo "[forge.INFO] Finished installing MySQL $F_MYSQL_VERSION"
+
+    mysql --version
+}
+
+case "$F_MYSQL_VERSION" in
+    "8.0")
+        mysql80
+    ;;
+    *)
+        echo "[forge.ERROR] Unsupported MySQL version $F_MYSQL_VERSION"
+        exit 1
+    ;;
+esac
